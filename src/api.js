1c86f84a855cd874686c8a7bdcbff83105ff2154
9620012072005 <chellammal786@gmail.com>
2025-02-12 09:25:10 +0000

    Updated API URL for deployment

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL + "/api" || "http://localhost:5000/api",
+  withCredentials: true, // Ensure cookies (like JWT) are sent
 });
 
 export default api;
