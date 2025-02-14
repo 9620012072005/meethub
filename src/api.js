fc08728f97d0569109cd14ffd7a2f0d0185d05cd
9620012072005 <chellammal786@gmail.com>
2025-02-14 18:09:18 +0000

    correct the register.s

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://meethub-backend.onrender.com/api"; // ✅ Backend URL (Ensure /api is included)
+const API_BASE_URL = "https://meethub-backend.onrender.com"; // ✅ Backend URL (Ensure /api is included)
 
 const api = axios.create({
   baseURL: API_BASE_URL,
